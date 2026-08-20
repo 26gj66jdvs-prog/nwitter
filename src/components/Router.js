c9008159cc74f7a99771a996363fd65d88edc1ae
김한주 <hanjukim@illumistone.local>
2026-08-21 00:29:22 +0900

    Router setup and authentication state

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,19 @@
-import { HashRouter as Router, Route, Switch} from "react-router-dom";
-
+import { useState } from "react";
+import { HashRouter as Router, Route, Routes} from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
 
 const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
     return(
         <Router>
-            <Switch>
-                <Route>
-
-                </Route>
-            </Switch>
+            <Routes>
+                {isLoggedIn ? (
+                    <Route path="/" element={<Home />} />
+                ) : (
+                    <Route path="/" element={<Auth />} />
+                )}
+            </Routes>
         </Router>
     );
 };
